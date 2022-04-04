7630d794b7ed7a0efd8afb8ea19eae6e080c17fd
TungVu858 <tungvt858@gmail.com>
2022-04-04 17:09:38 +0700

    done

--- src/Display_Hinh.java
+++ src/Display_Hinh.java
@@ -12,8 +12,37 @@ public class Display_Hinh {
             System.out.println("4.Exit");
             select = sc.nextInt();
             switch (select){
-
+                case 1:
+                    for (int i=0;i<3;i++){
+                            System.out.println("# # # # # # #");
+                        }
+                    break;
+                case 2:
+                    for (int i =1;i<=5;i++){
+                        for (int j =1;j<=i;j++){
+                            System.out.printf("# ");
+                        }
+                        System.out.print("\n");
+                    }
+                    break;
+                case 3:
+                    for (int i =1;i<=5;i++){
+                        for (int j =1;j<=i;j++){
+                            System.out.printf("# ");
+                        }
+                        System.out.print("\n");
+                    }
+                    for (int i =5;i>=1;i--){
+                        for (int j = 1;j<=i;j++){
+                            System.out.printf("# ");
+                        }
+                        System.out.print("\n");
+                    }
+                    break;
+                case 4:
+                    System.exit(0);
+                    }
             }
         }
     }
-}
+
